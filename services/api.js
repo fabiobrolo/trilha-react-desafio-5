85eb71886ebca721b1377b0f49fb08fa44082f1f
Fabio Brolo <fabiobrolo>
2024-10-01 20:35:18 -0300

    Desafio concluído

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://zxrzdjhydpzaikkjhwja.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4cnpkamh5ZHB6YWlra2pod2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MjMxNzEsImV4cCI6MjA0MzM5OTE3MX0.BtLh_B8vq4nW49NCxP61rBFQUOCAAe2R5k_CpzP3rw8",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4cnpkamh5ZHB6YWlra2pod2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MjMxNzEsImV4cCI6MjA0MzM5OTE3MX0.BtLh_B8vq4nW49NCxP61rBFQUOCAAe2R5k_CpzP3rw8"
     }
 })
